e7c9eb2502e8e6a039de7a30d3054785b97c1faf
haruki <meteo.haru@gmail.com>
2024-03-21 16:04:33 +0000

    impl Spinner

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,5 @@ export { Card } from './components/Card/Card.tsx';
 export { Input } from './components/Input/Input.tsx';
 export { Link } from './components/Link/Link.tsx';
 export { SearchInput } from './components/SearchInput/SearchInput.tsx';
+export { Spinner } from './components/Spinner/Spinner.tsx';
 export { Typo } from './components/Typography/Typo.tsx';
---
+++ src/stories/Spinner.stories.ts
@@ -0,0 +1,70 @@
+import type { Meta, StoryObj } from 'storybook-solidjs';
+import { Spinner } from '../components/Spinner/Spinner';
+
+const meta = {
+    title: 'Components/Feedback/Spinner',
+    component: Spinner,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        size: {
+            control: {
+                type: 'select',
+                options: ['small', 'medium', 'large'],
+            },
+        },
+        color: {
+            control: {
+                type: 'select',
+                options: ['primary', 'normal'],
+            },
+        },
+        darkMode: {
+            control: {
+                type: 'boolean',
+            },
+        },
+    },
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        size: 'medium',
+        color: 'normal',
+    },
+};
+
+export const Primary: Story = {
+    args: {
+        size: 'medium',
+        color: 'primary',
+    },
+};
+
+export const Small: Story = {
+    args: {
+        size: 'small',
+        color: 'normal',
+    },
+};
+
+export const Large: Story = {
+    args: {
+        size: 'large',
+        color: 'primary',
+    },
+};
+
+export const DarkMode: Story = {
+    args: {
+        size: 'medium',
+        color: 'normal',
+        darkMode: true,
+    },
+};
